bb35130c88d96512998c6c7791748d35aecf56c0
okocjan <oskar.kocjan@smcebi.edu.pl>
2022-01-20 16:35:59 +0100

    setup-okocjan added

---
+++ setup-okocjan.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dev-okocjan",
+    description="Zadanie 8 i 9",
+    version="1.0",
+    author="okocjan",
+    author_email="oskar.kocjan@smcebi.edu.pl",
+    license="MIT",
+    packages=['Module']
+)
