8a31cc95042d687c1377412648c764adbd6acdb4
lkiedos <lukasz5041@gmail.com>
2022-01-20 18:50:13 +0100

    commit

---
+++ setup-lkiedos.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="Module",
+    description="Python function",
+    version="1.0",
+    author="lkiedos",
+    author_email="none",
+    packages=['Module'],
+)
