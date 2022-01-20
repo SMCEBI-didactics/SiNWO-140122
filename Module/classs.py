51e0fb094665a2bec6293230e8ff77771edcf1d9
OliwierP <oliwier.pszeniczko@smcebi.edu.pl>
2022-01-21 00:35:05 +0100

    pdoc&.gitigore

--- Module/classs.py
+++ Module/classs.py
@@ -3,6 +3,9 @@
 
 class Vector():
     """
+
+    Klasa Vector reprezentuje wektor trojwymiarowy
+
     """
     def __init__(self):
         self.x = 0
@@ -11,6 +14,17 @@ class Vector():
 
     def add(self, x, y, z):
         """
+        
+        Metoda add zwieksza skladowe wektora o podane wartosci
+
+        Args:
+            x: wartosc typu liczbowego, pierwsza skladowa wektora
+            y: wartosc typu liczbowego, druga skladowa wektora
+            z: wartosc typu liczbowego, trzeci skladowa wektora
+
+        Returs:
+            None
+
         """
         self.x += x
         self.y += y
--- Module/func.py
+++ Module/func.py
@@ -2,10 +2,23 @@ from time import time as t
 
 def f(x):
     """
+
+    Funkcja f wyliczajaca wartosc podanej funkcji kwadratowej
+    
+    Args:
+        x: wartosc typu liczbowego
+
+    Returns: wartosc podanej funkcji dla parametru x
     """
     return x**2 + x + 3
 
 def time():
     """
+    
+    Funkcja time przedstawia czas
+
+    Returns:
+        t(): wartosc typu liczbowego
+
     """
     return t()
