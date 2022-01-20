c7e25f7381f8ab34f9bd6c2a33a4d784ead0bbcc
awowra0 <adam.wowra@smcebi.edu.pl>
2022-01-20 13:29:22 +0100

    Added setup-login.py

---
+++ setup-awowra.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="Module",
+    description="Simple python class and functions.",
+    version="1.0",
+    author="aw",
+    author_email="none",
+    packages=['Module']
+)
