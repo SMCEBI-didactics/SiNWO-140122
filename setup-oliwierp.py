33c6a364b18b0bb4a93ee7c0a74f4497ff61ecb1
OliwierP <oliwier.pszeniczko@smcebi.edu.pl>
2022-01-21 00:17:43 +0100

    output.txt&requirements.txt

--- setup-oliwierp.py
+++ setup-oliwierp.py
@@ -1,9 +1,11 @@
+from setuptools import setup
+
 setup(
 	name="Module",
-	description="Zadanie 8"
-	version="1.0"
-	author="oliwierp"
-	author_emial="oliwier.pszeniczko@smcebi.edu.pl"
-	packages['Module']
+	description="Zadanie 8",
+	version="1.0",
+	author="oliwierp",
+	author_emial="oliwier.pszeniczko@smcebi.edu.pl",
+	packages=['Module'],
     entry_points={'console_scripts': ['zadanie8-oliwierp = Module.func:time']}
 )
