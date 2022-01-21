b78024c2e3fb203abc50531b0a27421910403a81
Adrian Moskal <adrian.moskal@icloud.com>
2022-01-21 01:09:25 +0100

    Add setup file

---
+++ setup-amoskal.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages
+
+setup(
+    name='setup-file',
+    version='1.0.0',
+    description='short description of setup file',
+    author='Adrian Moskal',
+    author_email='adrian.moskal@smcebi.edu.pl',
+    packages=['Module']
+)
